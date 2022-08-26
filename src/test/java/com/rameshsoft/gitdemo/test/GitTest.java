5740585dddd46e1bfcae367683c60929ec704666
srkrishna3384 <srkrishna3384@gmail.com>
2022-08-26 11:00:54 +0530

    Added GitAugustBatch project

---
+++ src/test/java/com/rameshsoft/gitdemo/test/GitTest.java
@@ -0,0 +1,23 @@
+package com.rameshsoft.gitdemo.test;
+
+import org.testng.annotations.Test;
+
+public class GitTest {
+	
+	@Test
+	public void testOne() {
+		System.out.println("TestOne method....");
+	}
+	
+	@Test
+	public void testTwo() {
+		System.out.println("TestTwo method....");
+	}
+	
+	@Test
+	public void testThree() {
+		System.out.println("TestThree method....");
+	}
+
+
+}
