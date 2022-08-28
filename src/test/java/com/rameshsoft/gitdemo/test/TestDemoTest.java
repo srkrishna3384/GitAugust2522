b1a6de8f57a81638df4421bce9a2cf60dbd979d5
srkrishna3384 <srkrishna3384@gmail.com>
2022-08-28 11:04:04 +0530

    Added TestDemoTest file

---
+++ src/test/java/com/rameshsoft/gitdemo/test/TestDemoTest.java
@@ -0,0 +1,12 @@
+package com.rameshsoft.gitdemo.test;
+
+import org.testng.annotations.Test;
+
+public class TestDemoTest {
+	
+	@Test
+	public void testFour() {
+		System.out.println("TestFour..........");
+	}
+
+}
